5fb2dd86b6ff70ca8fe6781660258d862ae9ebf7
Devin Beliveau <devinmarieb@gmail.com>
2017-05-09 11:19:39 -0600

    Add server

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,17 @@ module.exports = {
       { test: /\.scss$/, loader: 'style!css!sass' },
     ],
   },
+    devServer: {
+    contentBase: './build',
+    inline: true
+  },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json', '.scss', '.css'],
   },
+  node: {
+    fs: "empty",
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  }
 };
